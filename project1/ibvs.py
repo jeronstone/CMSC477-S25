89c56d4797dea8999fa70cd292bfa10effe0b181
Joshua Stone <99224714+jeronstone@users.noreply.github.com>
2025-02-10 16:29:23 -0500

    p1 progress

--- project1/ibvs.py
+++ project1/ibvs.py
@@ -2,10 +2,10 @@ import numpy as np
 from ApriltagDetector import get_pose_apriltag_in_camera_frame
 
 # @param detection is one element from list returned by ApriltagDetector.find_tags()
-def get_ibvs_speeds(detection):
+def get_ibvs_speeds(detection, offset):
 
     t_ca, R_ca = get_pose_apriltag_in_camera_frame(detection)
-    #print('t_ca', t_ca)
+    print('t_ca', t_ca)
     #print('R_ca', R_ca)
 
     x = t_ca[0] / t_ca[2]
@@ -13,11 +13,12 @@ def get_ibvs_speeds(detection):
 
     Lx = np.matrix([[x,-1*(1+x**2)], [y,-1*x*y]])
 
-    deltas = np.matrix([[(t_ca[2] - 1)], [(t_ca[0])]])
+    deltas = np.matrix([[(t_ca[2] - offset[0])], [(t_ca[0]) - offset[1]]])
 
-    lam = np.matrix([[-75, 0], [0, -3]])
+    #lam = np.matrix([[-75, 0], [0, -3]])
+    lam = np.matrix([[-3, 0], [0, -1]])
 
     res = np.matmul(lam, np.matmul(Lx.I, deltas))
 
     #print(f'{res.item(1)} \t {res.item(0)}')
-    return res
+    return res, deltas
--- project1/main.py
+++ project1/main.py
@@ -6,6 +6,20 @@ from queue import Empty
 from robomaster import robot
 from robomaster import camera
 from ApriltagDetector import *
+from ibvs import get_ibvs_speeds
+
+def draw_detections(frame, detections):
+    for detection in detections:
+        pts = detection.corners.reshape((-1, 1, 2)).astype(np.int32)
+
+        frame = cv2.polylines(frame, [pts], isClosed=True, color=(0, 0, 255), thickness=2)
+
+        top_left = tuple(pts[0][0])  # First corner
+        top_right = tuple(pts[1][0])  # Second corner
+        bottom_right = tuple(pts[2][0])  # Third corner
+        bottom_left = tuple(pts[3][0])  # Fourth corner
+        cv2.line(frame, top_left, bottom_right, color=(0, 0, 255), thickness=2)
+        cv2.line(frame, top_right, bottom_left, color=(0, 0, 255), thickness=2)
 
 if __name__ == '__main__':
     # More legible printing from numpy.
@@ -20,3 +34,51 @@ if __name__ == '__main__':
     K = np.array([[314, 0, 320], [0, 314, 180], [0, 0, 1]]) # Camera focal length and center pixel
     marker_size_m = 0.153 # Size of the AprilTag in meters
     apriltag = AprilTagDetector(K, threads=2, marker_size_m=marker_size_m)
+
+    offsets = [
+        (33, (1, 0)), 
+        (33, (2, 0)),
+        (33, (2, 1))
+    ]
+    curr = 0
+
+    while (True):
+        try:
+            img = ep_camera.read_cv2_image(strategy="newest", timeout=0.5)
+        except Empty:
+            time.sleep(0.001)
+            continue
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray.astype(np.uint8)
+
+        detections = apriltag.find_tags(gray)
+
+        if curr < len(offsets):
+
+            if len(detections) < 1:
+                ep_chassis.drive_speed(x=0, y=0, z=0, timeout=5)
+
+            for detect in detections:
+                if detect.tag_id == offsets[curr][0]:
+                    res, deltas = get_ibvs_speeds(detect, offsets[curr][1])
+
+                    error = np.linalg.norm(deltas)
+                    #print(f'Error: {error}')
+
+                    if error > 0.05:
+                        ep_chassis.drive_speed(x=res.item(1), y=0, z=res.item(0), timeout=5)
+                    else:
+                        ep_chassis.drive_speed(x=0, y=0, z=0, timeout=5)
+                        curr += 1
+                        print('curr ++')
+                        break
+
+        else:
+            ep_chassis.drive_speed(x=0, y=0, z=0, timeout=5)
+        
+        draw_detections(img, detections)
+        cv2.imshow("img", img)
+        if cv2.waitKey(1) == ord('q'):
+            ep_chassis.drive_speed(x=0, y=0, z=0, timeout=5)
+            break
