ab400b813296b6c41dead79fc4bb24ba7235618b
Jay Rana <jay.rana1@gmail.com>
2025-02-17 14:47:59 -0500

    Added Dijkstra code

---
+++ project1/dijkstra.py
@@ -0,0 +1,148 @@
+# jrana dijkstra.py 2025/02/13
+
+from map import *
+from PIL import Image
+import os
+from collections import deque
+import math
+
+CROSS_CORNERS = False # whether or not the path can cross an obstacle corner while traversing, since robots would collide with the corner in the real world
+ITERATIONS_PER_FRAME = 32 # how many iterations occur before we generate a new frame of the GIF 
+
+def Dijkstra(map, gen_frames, frames, scale=1):
+    q = [] # priority queue with priority = dist from source
+
+    # set distance of start to 0 and add it to the queue
+    map.map[map.start[1]][map.start[0]].dist = 0
+    q.append((map.start, 0))
+
+    anim_count = 0 # GIF animation control
+
+    while q.count != 0:
+        # get current node from priority queue (i.e. node in queue with shortest distance to start) and mark it as visited
+        curr_node = min(q, key=lambda p:p[1])
+        q.remove(curr_node)
+        curr_node = curr_node[0] # get the coordinates
+        map.map[curr_node[1]][curr_node[0]].visited = True
+
+        # if this is the goal, return it and end Dijkstra
+        if curr_node == map.goal:
+            return curr_node
+
+        # check every neighbor, including diagonals
+        neighbors = []
+        include_neighbors = [[1, 1, 1],
+                             [1, 0, 1],
+                             [1, 1, 1]]
+        # top left
+        new_x = curr_node[0] - 1
+        new_y = curr_node[1] - 1
+        top_left = (new_x, new_y)
+        if not(new_x >= 0 and new_y >= 0 and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+            include_neighbors[0][0] = 0
+        # top middle
+        new_x = curr_node[0]
+        new_y = curr_node[1] - 1
+        top_middle = (new_x, new_y)
+        if not(new_y >= 0 and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+            include_neighbors[0][1] = 0
+        # if we can't cross corners and this is an obstacle, exclude all neighbors next to this one
+        if CROSS_CORNERS is False and map.map[new_y][new_x].square_type is MapSquareEnum.OBSTACLE:
+            include_neighbors[0][0] = 0
+            include_neighbors[0][1] = 0
+            include_neighbors[0][2] = 0
+        # top right
+        new_x = curr_node[0] + 1
+        new_y = curr_node[1] - 1
+        top_right = (new_x, new_y)
+        if not(new_x < map.num_cols and new_y >= 0 and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+            include_neighbors[0][2] = 0
+        # middle left
+        new_x = curr_node[0] - 1
+        new_y = curr_node[1]
+        middle_left = (new_x, new_y)
+        if not(new_x >= 0 and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+            include_neighbors[1][0] = 0
+        # if we can't cross corners and this is an obstacle, exclude all neighbors next to this one
+        if CROSS_CORNERS is False and map.map[new_y][new_x].square_type is MapSquareEnum.OBSTACLE:
+            include_neighbors[0][0] = 0
+            include_neighbors[1][0] = 0
+            include_neighbors[2][0] = 0
+        # middle right
+        new_x = curr_node[0] + 1
+        new_y = curr_node[1]
+        middle_right = (new_x, new_y)
+        if not(new_x < map.num_cols and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+            include_neighbors[1][2] = 0
+        # if we can't cross corners and this is an obstacle, exclude all neighbors next to this one
+        if CROSS_CORNERS is False and map.map[new_y][new_x].square_type is MapSquareEnum.OBSTACLE:
+            include_neighbors[0][2] = 0
+            include_neighbors[1][2] = 0
+            include_neighbors[2][2] = 0
+        # bottom left
+        new_x = curr_node[0] - 1
+        new_y = curr_node[1] + 1
+        bottom_left = (new_x, new_y)
+        if not(new_x >= 0 and new_y < map.num_rows and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+            include_neighbors[2][0] = 0
+        # bottom middle
+        new_x = curr_node[0]
+        new_y = curr_node[1] + 1
+        bottom_middle = (new_x, new_y)
+        if not(new_y < map.num_rows and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+            include_neighbors[2][1] = 0
+        # if we can't cross corners and this is an obstacle, exclude all neighbors next to this one
+        if CROSS_CORNERS is False and map.map[new_y][new_x].square_type is MapSquareEnum.OBSTACLE:
+            include_neighbors[2][0] = 0
+            include_neighbors[2][1] = 0
+            include_neighbors[2][2] = 0
+        # bottom right
+        new_x = curr_node[0] + 1
+        new_y = curr_node[1] + 1
+        bottom_right = (new_x, new_y)
+        if not(new_x < map.num_cols and new_y < map.num_rows and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+            include_neighbors[2][2] = 0
+
+        # add included neighbors to list; if second element is 1 then it is a diagonal, otherwise it is not a diagonal
+        if include_neighbors[0][0] == 1:
+            neighbors.append((top_left, 1))
+        if include_neighbors[0][1] == 1:
+            neighbors.append((top_middle, 0))
+        if include_neighbors[0][2] == 1:
+            neighbors.append((top_right, 1))
+        if include_neighbors[1][0] == 1:
+            neighbors.append((middle_left, 0))
+        if include_neighbors[1][2] == 1:
+            neighbors.append((middle_right, 0))
+        if include_neighbors[2][0] == 1:
+            neighbors.append((bottom_left, 1))
+        if include_neighbors[2][1] == 1:
+            neighbors.append((bottom_middle, 0))
+        if include_neighbors[2][2] == 1:
+            neighbors.append((bottom_right, 1))
+        
+        # go through list of neighbors
+        for neighbor in neighbors:
+            new_dist = map.map[curr_node[1]][curr_node[0]].dist
+            # new distance will either be old distance + 1 or old distance + sqrt(2) if diagonal
+            if neighbor[1] == 0:
+                new_dist += 1.0
+            else:
+                new_dist += math.sqrt(2)
+            neighbor = neighbor[0] # get coordinates
+            # if we have found a new minimum distance to this neighbor, update the distance and parent
+            if new_dist < map.map[neighbor[1]][neighbor[0]].dist:
+                map.map[neighbor[1]][neighbor[0]].parent = curr_node
+                map.map[neighbor[1]][neighbor[0]].dist = new_dist
+                q.append((neighbor, new_dist))
+        
+        # GIF animation control
+        anim_count += 1
+        if gen_frames == True and anim_count >= ITERATIONS_PER_FRAME - 1:
+            frames.append(map.gen_img(scale=scale))
+            anim_count = 0
+
+map1 = Map(os.getcwd() + "\\Project1.csv")
+Dijkstra(map, False, [], scale=10)
+map1.draw_path()
+map1.gen_img(scale=20).show()
---
+++ project1/map.py
@@ -0,0 +1,96 @@
+from enum import Enum
+import csv
+import os
+from PIL import Image, ImageDraw
+
+COLOR_FREE = (255, 255, 255)
+COLOR_OBSTACLE = (0, 0, 0)
+COLOR_START = (0, 255, 0)
+COLOR_GOAL = (255, 0, 0)
+COLOR_VISITED = (127, 127, 127)
+COLOR_PATH = (255, 255, 0)
+
+def get_color(square_type):
+    if square_type.value == 0:
+        return COLOR_FREE
+    if square_type.value == 1:
+        return COLOR_OBSTACLE
+    if square_type.value == 2:
+        return COLOR_START
+    if square_type.value == 3:
+        return COLOR_GOAL
+
+class MapSquareEnum(Enum):
+    FREE = 0
+    OBSTACLE = 1
+    START = 2
+    GOAL = 3
+
+class MapSquare:
+    def __init__(self, square_type=MapSquareEnum(0)):
+        self.square_type = MapSquareEnum(square_type)
+        self.visited = False
+        self.in_path = False
+        self.parent = (-1, -1)
+        self.dist = float('inf')
+
+class Map:
+    def __init__(self, map_csv_filepath):
+        self.map = [] # 2D array of MapSquares
+        # x is the horizontal value (#cols)
+        # y is the vertical value (#rows)
+        self.start = (-1, -1) # (x,y) of start position
+        self.goal = (-1, -1) # (x,y) of goal position
+        self.num_rows = -1 # number of rows in map
+        self.num_cols = -1 # number of columns in map
+        with open(map_csv_filepath, newline='') as map_csv:
+            print("Map file loaded from", map_csv_filepath)
+            map_csv_reader = csv.reader(map_csv, delimiter=',')
+            j = 0
+            for row in map_csv_reader:
+                map_row = []
+                i = 0
+                for entry in row:
+                    if entry == '':
+                        entry = MapSquareEnum.FREE.value
+                    map_row.append(MapSquare(square_type=int(entry)))
+                    if int(entry) == MapSquareEnum.START.value:
+                        self.start = (i, j)
+                    if int(entry) == MapSquareEnum.GOAL.value:
+                        self.goal = (i, j)
+                    i += 1
+                self.map.append(map_row)
+                j += 1
+            self.num_rows = j
+            self.num_cols = i
+            print("Map initialized. Size =", self.num_rows, "rows x", self.num_cols, "cols. Start =", self.start, ". Goal =", self.goal, ".")
+
+    def visit(self, x, y, parent_x, parent_y):
+        self.map[y][x].visited = True
+        self.map[y][x].parent = (parent_x, parent_y)
+
+    def draw_path(self):
+        curr_cell = self.goal
+        while curr_cell != self.start:
+            x = curr_cell[0]
+            y = curr_cell[1]
+            self.map[y][x].in_path = True
+            curr_cell = self.map[y][x].parent
+    
+    def gen_img(self, scale=1):
+        frame = Image.new("RGB", (self.num_cols * scale, self.num_rows * scale), COLOR_FREE)
+        frame_draw = ImageDraw.Draw(frame)
+        for cols in range(0, self.num_cols):
+            scaled_cols = cols * scale
+            for rows in range(0, self.num_rows):
+                scaled_rows = rows * scale
+                frame_draw.rectangle([(scaled_cols, scaled_rows), (scaled_cols + scale - 1, scaled_rows + scale - 1)], fill=get_color(self.map[rows][cols].square_type))
+                if self.map[rows][cols].visited and self.map[rows][cols].square_type is not MapSquareEnum.START and self.map[rows][cols].square_type is not MapSquareEnum.GOAL:
+                    frame_draw.rectangle([(scaled_cols, scaled_rows), (scaled_cols + scale - 1, scaled_rows + scale - 1)], fill=COLOR_VISITED)
+                if self.map[rows][cols].in_path and self.map[rows][cols].square_type is not MapSquareEnum.START and self.map[rows][cols].square_type is not MapSquareEnum.GOAL:
+                    frame_draw.rectangle([(scaled_cols, scaled_rows), (scaled_cols + scale - 1, scaled_rows + scale - 1)], fill=COLOR_PATH)
+        return frame
+
+if __name__ == "__main__":
+    map1 = Map(os.getcwd() + "\\Maps\\Map1.csv")
+    map1.gen_img(scale=10).show()
