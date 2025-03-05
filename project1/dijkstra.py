dcd292784e79a16ae54ee92942bf252e99da1c1e
Jay Rana <jay.rana1@gmail.com>
2025-03-05 17:10:28 -0500

    improved Dijkstra with subdivisions and obstacle avoidance

--- project1/dijkstra.py
+++ project1/dijkstra.py
@@ -16,7 +16,7 @@ def Dijkstra(map, gen_frames, frames, scale=1):
     q = [] # priority queue with priority = dist from source
 
     # set distance of start to 0 and add it to the queue
-    map.map[map.start[1]][map.start[0]].dist = 0
+    map.getSquareNoScale(*map.start).dist = 0
     q.append((map.start, 0))
 
     anim_count = 0 # GIF animation control
@@ -26,7 +26,9 @@ def Dijkstra(map, gen_frames, frames, scale=1):
         curr_node = min(q, key=lambda p:p[1])
         q.remove(curr_node)
         curr_node = curr_node[0] # get the coordinates
-        map.map[curr_node[1]][curr_node[0]].visited = True
+        # curr_x, curr_y = array_to_grid(*curr_node)
+        # curr_node = (curr_x, curr_y)
+        map.getSquareNoScale(*curr_node).visited = True
 
         # if this is the goal, return it and end Dijkstra
         if curr_node == map.goal:
@@ -41,13 +43,13 @@ def Dijkstra(map, gen_frames, frames, scale=1):
         new_x = curr_node[0] - 1
         new_y = curr_node[1] - 1
         top_left = (new_x, new_y)
-        if not(new_x >= 0 and new_y >= 0 and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+        if not(new_x >= 0 and new_x < map.map_num_cols and new_y >= 0 and new_y < map.map_num_rows and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE_SOFT):
             include_neighbors[0][0] = 0
         # top middle
         new_x = curr_node[0]
         new_y = curr_node[1] - 1
         top_middle = (new_x, new_y)
-        if not(new_y >= 0 and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+        if not(new_x >= 0 and new_x < map.map_num_cols and new_y >= 0 and new_y < map.map_num_rows and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE_SOFT):
             include_neighbors[0][1] = 0
         # if we can't cross corners and this is an obstacle, exclude all neighbors next to this one
             if CROSS_CORNERS is False:
@@ -58,13 +60,13 @@ def Dijkstra(map, gen_frames, frames, scale=1):
         new_x = curr_node[0] + 1
         new_y = curr_node[1] - 1
         top_right = (new_x, new_y)
-        if not(new_x < map.num_cols and new_y >= 0 and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+        if not(new_x >= 0 and new_x < map.map_num_cols and new_y >= 0 and new_y < map.map_num_rows and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE_SOFT):
             include_neighbors[0][2] = 0
         # middle left
         new_x = curr_node[0] - 1
         new_y = curr_node[1]
         middle_left = (new_x, new_y)
-        if not(new_x >= 0 and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+        if not(new_x >= 0 and new_x < map.map_num_cols and new_y >= 0 and new_y < map.map_num_rows and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE_SOFT):
             include_neighbors[1][0] = 0
         # if we can't cross corners and this is an obstacle, exclude all neighbors next to this one
             if CROSS_CORNERS is False:
@@ -75,7 +77,7 @@ def Dijkstra(map, gen_frames, frames, scale=1):
         new_x = curr_node[0] + 1
         new_y = curr_node[1]
         middle_right = (new_x, new_y)
-        if not(new_x < map.num_cols and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+        if not(new_x >= 0 and new_x < map.map_num_cols and new_y >= 0 and new_y < map.map_num_rows and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE_SOFT):
             include_neighbors[1][2] = 0
         # if we can't cross corners and this is an obstacle, exclude all neighbors next to this one
             if CROSS_CORNERS is False:
@@ -86,13 +88,13 @@ def Dijkstra(map, gen_frames, frames, scale=1):
         new_x = curr_node[0] - 1
         new_y = curr_node[1] + 1
         bottom_left = (new_x, new_y)
-        if not(new_x >= 0 and new_y < map.num_rows and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+        if not(new_x >= 0 and new_x < map.map_num_cols and new_y >= 0 and new_y < map.map_num_rows and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE_SOFT):
             include_neighbors[2][0] = 0
         # bottom middle
         new_x = curr_node[0]
         new_y = curr_node[1] + 1
         bottom_middle = (new_x, new_y)
-        if not(new_y < map.num_rows and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+        if not(new_x >= 0 and new_x < map.map_num_cols and new_y >= 0 and new_y < map.map_num_rows and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE_SOFT):
             include_neighbors[2][1] = 0
         # if we can't cross corners and this is an obstacle, exclude all neighbors next to this one
             if CROSS_CORNERS is False:
@@ -103,7 +105,7 @@ def Dijkstra(map, gen_frames, frames, scale=1):
         new_x = curr_node[0] + 1
         new_y = curr_node[1] + 1
         bottom_right = (new_x, new_y)
-        if not(new_x < map.num_cols and new_y < map.num_rows and map.map[new_y][new_x].square_type is not MapSquareEnum.OBSTACLE):
+        if not(new_x >= 0 and new_x < map.map_num_cols and new_y >= 0 and new_y < map.map_num_rows and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE and map.getSquareNoScale(new_x,new_y).square_type is not MapSquareEnum.OBSTACLE_SOFT):
             include_neighbors[2][2] = 0
 
         # add included neighbors to list; if second element is 1 then it is a diagonal, otherwise it is not a diagonal
@@ -126,7 +128,7 @@ def Dijkstra(map, gen_frames, frames, scale=1):
         
         # go through list of neighbors
         for neighbor in neighbors:
-            new_dist = map.map[curr_node[1]][curr_node[0]].dist
+            new_dist = map.getSquareNoScale(*curr_node).dist
             # new distance will either be old distance + 1 or old distance + sqrt(2) if diagonal
             if neighbor[1] == 0:
                 new_dist += 1.0
@@ -134,9 +136,9 @@ def Dijkstra(map, gen_frames, frames, scale=1):
                 new_dist += math.sqrt(2)
             neighbor = neighbor[0] # get coordinates
             # if we have found a new minimum distance to this neighbor, update the distance and parent
-            if new_dist < map.map[neighbor[1]][neighbor[0]].dist:
-                map.map[neighbor[1]][neighbor[0]].parent = curr_node
-                map.map[neighbor[1]][neighbor[0]].dist = new_dist
+            if new_dist < map.getSquareNoScale(*neighbor).dist:
+                map.getSquareNoScale(*neighbor).parent = curr_node
+                map.getSquareNoScale(*neighbor).dist = new_dist
                 q.append((neighbor, new_dist))
         
         # GIF animation control
@@ -149,20 +151,24 @@ def solve_maze(show=False):
     map1 = Map(os.getcwd() + "/Project1.csv")
     Dijkstra(map1, False, [], scale=10)
     path = map1.draw_path()
-    x_pts = [p[0]-0.5 for p in path]
-    y_pts = [p[1]-0.5 for p in path]
+    x_pts = []
+    y_pts = []
+    for p in path:
+        scaled_x, scaled_y = array_to_grid(*p)
+        x_pts.append(scaled_x)
+        y_pts.append(scaled_y)
     t = np.linspace(0, 1, num=len(path))
     t_spl = np.linspace(0, 1, num=100)
     x_spl = CubicSpline(t, x_pts)
     y_spl = CubicSpline(t, y_pts)
-
+    
     if show:
         fig, ax = plt.subplots(1, 1)
         ax.xaxis.set_ticks_position('top')
-        ax.set_xlim([0, map1.num_cols])
-        ax.set_ylim([map1.num_rows, 0])
-        plt.xticks(np.arange(0, map1.num_cols, 1))
-        plt.yticks(np.arange(0, map1.num_rows, 1))
+        ax.set_xlim([0, map1.map_num_cols//NUM_SUBDIVISIONS])
+        ax.set_ylim([map1.map_num_rows//NUM_SUBDIVISIONS, 0])
+        plt.xticks(np.arange(0, map1.map_num_cols//NUM_SUBDIVISIONS, 1))
+        plt.yticks(np.arange(0, map1.map_num_rows//NUM_SUBDIVISIONS, 1))
         plt.grid('True')
         ax.plot(x_spl(t_spl), y_spl(t_spl))
         # ax[1].plot(x_spl(t), y_spl(t))
