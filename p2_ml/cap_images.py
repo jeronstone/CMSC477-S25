f627f566d4cfb11273f3c8c5ae58543dd7b8a9cf
Joshua Stone <jstone14@terpmail.umd.edu>
2025-03-31 16:13:04 -0400

    project 2 init

---
+++ p2_ml/cap_images.py
@@ -0,0 +1,40 @@
+import cv2
+from robomaster import robot
+from robomaster import camera
+
+i=0
+
+def save_image(image, filename="floor"):
+    global i
+    cv2.imwrite(f'train_images/{filename}_{i}.png', image)
+    i+=1
+    print(f"Image saved")
+
+def on_button_click(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        save_image(param)
+
+# Use vid instead of ep_camera to use your laptop's webcam
+# vid = cv2.VideoCapture(0)
+
+
+ep_robot = robot.Robot()
+ep_robot.initialize(conn_type="sta", sn="3JKCH8800100YR")
+ep_camera = ep_robot.camera
+ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
+
+cv2.namedWindow("Camera Feed")
+
+while True:
+    # ret, frame = vid.read()
+    frame = ep_camera.read_cv2_image()
+    if frame is not None:
+        cv2.setMouseCallback("Camera Feed", on_button_click, frame)
+        cv2.imshow("Camera Feed", frame)
+        
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+
+
---
+++ p2_ml/yolotest.py
@@ -0,0 +1,62 @@
+from ultralytics import YOLO
+import cv2
+import time
+from robomaster import robot
+from robomaster import camera
+
+
+print('model')
+model = YOLO(r"C:\Users\jesto\Desktop\CMSC477\CMSC477-S25\runs\detect\train5\weights\best.pt")
+
+
+# Use vid instead of ep_camera to use your laptop's webcam
+# vid = cv2.VideoCapture(0)
+
+
+ep_robot = robot.Robot()
+ep_robot.initialize(conn_type="sta", sn="3JKCH8800100YR")
+ep_camera = ep_robot.camera
+ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
+
+
+while True:
+    # ret, frame = vid.read()
+    frame = ep_camera.read_cv2_image(strategy="newest", timeout=0.5)
+    if frame is not None:
+        start = time.time()
+        if model.predictor:
+            model.predictor.args.verbose = False
+        result = model.predict(source=frame, show=False)[0]
+        
+        #print(result)
+
+        # DIY visualization is much faster than show=True for some reason
+        boxes = result.boxes
+        for box in boxes:
+            xyxy = box.xyxy.cpu().numpy().flatten()
+            cv2.rectangle(frame,
+                          (int(xyxy[0]), int(xyxy[1])), 
+                          (int(xyxy[2]), int(xyxy[3])),
+                           color=(0, 0, 255), thickness=2)
+            
+            corners = [int(xyxy[0]), int(xyxy[1]), int(xyxy[2]), int(xyxy[3])]
+            corners[0] = (corners[0] - 640/2) / 640
+            corners[2] = (corners[2] - 640/2) / 640
+            
+            corners[1] = (corners[1] - 360/2) / 360
+            corners[3] = (corners[3] - 360/2) / 360
+            
+            print(corners)
+
+        cv2.imshow('frame', frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+        # print(results)
+
+
+        end = time.time()
+        #print(1.0 / (end-start))
+
+
