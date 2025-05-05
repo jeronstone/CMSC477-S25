a6afbc97c994ebe5eaef246488d868cac4c5c7c9
Joshua Stone <jstone14@terpmail.umd.edu>
2025-05-04 22:14:20 -0400

    added class to vision, added move to xy (needs testing)

--- project3/robot_controller.py
+++ project3/robot_controller.py
@@ -31,6 +31,7 @@ class Robot():
         self.vision = Vision(r"C:\Users\jesto\Desktop\CMSC477\CMSC477-S25\runs\detect\train2\weights\best.pt")
         self.ibvs_controller = IBVS_Controller(control_mode='2xz', interaction_mode='mean', num_pts=4)
         
+        
         self.controller.set_lambda_matrix([3.0, 1.25]) # robot y velocity; robot x velocity
         self.controller.set_desired_points([(-0.2, -0.7, 0.18), (0.2, -0.7, 0.18), (-0.2, 1.0, 0.18), (0.2, 1.0, 0.18)])
 
@@ -76,7 +77,9 @@ class Robot():
             self.ep_chassis.drive_speed(x=0, y=0, z=20, timeout=5)
         else:
         
-            corners, detected_block_lines_hough, depth = detections[0] 
+            # TODO determine which detection to follow
+            # blocks are cls = 2, 3, 4
+            cls, corners, detected_block_lines_hough, depth = detections[0]
                     
             controller.set_current_points([(corners[0], corners[1], depth), (corners[2], corners[1], depth), (corners[0], corners[3], depth), (corners[2], corners[3], depth)])
             controller.calculate_interaction_matrix()
@@ -124,4 +127,30 @@ class Robot():
             #if corners[1] > 0.06 and corners[3] > 0.95 and corners[0] > -0.2 and corners[2] < 0.2:
                 print('close to block, transition')
             print(f"horiz_ang: {most_horizontal_angle} depth: {depth} err_nrm: {err_nrm} vels: x {robot_x_velocity} y {robot_y_velocity}")
-        
+    
+    
+    DIST_THRESH_X = 1
+    DIST_THRESH_Y = 1
+    def move_to_xy(self, desired_x, desired_y):
+        err_x = self.our_position[0] - desired_x
+        err_y = self.our_position[1] - desired_y
+
+        while abs(err_x) > self.DIST_THRESH_X or abs(err_y) > self.DIST_THRESH_Y:
+            
+            err_x = self.our_position[0] - desired_x
+            err_y = self.our_position[1] - desired_y
+            
+            velo_x = 0.0
+            velo_y = 0.0
+            
+            if err_x > self.DIST_THRESH_X:
+                velo_x = math.copysign(2.0, err_x)
+                
+            if err_y > self.DIST_THRESH_Y:
+                velo_y = math.copysign(2.0, err_y)
+            
+            self.ep_chassis.drive_speed(x=velo_x, y=velo_y, z=0.0, timeout=5)
+            time.sleep(0.1)
+            
+        self.ep_chassis.drive_speed(x=0.0, y=0.0, z=0.0, timeout=5)
+            
